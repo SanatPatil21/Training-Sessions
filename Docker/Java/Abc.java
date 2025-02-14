29060731887af3e4573aec989c5af96abd2d2a0f
Sanat Patil <sanat.patil@wissen.com>
2025-02-14 13:12:21 +0530

    Started the Docker Topic

---
+++ Docker/Java/Abc.java
@@ -0,0 +1,14 @@
+public class Abc{
+
+	public static void main(String args[]){
+		System.out.println("Hello from Java Program");
+		System.out.println("This is compiled through Docker");
+		System.out.println("Also we have set the Environment Variables");
+		System.out.println("Hello from Java Program");
+		System.out.println("Hello from Java Program");
+
+
+	}
+
+
+}
